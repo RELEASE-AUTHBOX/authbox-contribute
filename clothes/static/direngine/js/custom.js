f9f22aa9c1ecc0a83a858f4261888a0d58cd2e4d
manjaro-linux <suratiwan03@gmail.com>
2023-09-02 12:17:33 +0800

    Detail and List pages complete

--- clothes/static/direngine/js/custom.js
+++ clothes/static/direngine/js/custom.js
@@ -7,3 +7,23 @@ function openWA(number, initial_text) {
     //else
     //   alert("This link will open Whatsapp on android");
  }
+
+ $(document).on("ready", function() {
+    /*[ Back to top ]*/
+    try {
+        var windowH = $(window).height()/2;
+
+        $(window).on('scroll',function(){
+            if ($(this).scrollTop() > windowH) {
+                $("#myBtn").addClass('show-btn-back-to-top');
+            } else {
+                $("#myBtn").removeClass('show-btn-back-to-top');
+            }
+        });
+
+        $('#myBtn').on("click", function(){
+            $('html, body').animate({scrollTop: 0}, 300);
+        });
+    } catch(er) {console.log(er);}
+    
+ });
