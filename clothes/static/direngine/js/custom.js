9bc8d1e963bf945a088b0526de025d0f5a45b16e
manjaro-linux <suratiwan03@gmail.com>
2023-09-06 10:16:46 +0800

    Test pyminify 12

--- clothes/static/direngine/js/custom.js
+++ clothes/static/direngine/js/custom.js
@@ -1,9 +1,13 @@
-function openWA(number, initial_text) {
+function openWA(number, initial_text, is_mobile=false) {
     //var number = document.getElementById("waNumber").value;
     //if (isMobile()=="true")
     // initial_text: Hai, Saya ada pertanyaan tentang cara membuat website di https://authbox.web.id/
-    console.log('number',number);
-    window.open("https://api.whatsapp.com/send?phone="+ number +"&text=" + initial_text,"_blank");
+    // console.log('number',number);
+    if (is_mobile) {
+        window.open("https://api.whatsapp.com/send?phone="+ number +"&text=" + initial_text,"_blank");
+    } else {
+        window.open("https://web.whatsapp.com/send?phone="+ number +"&text=" + initial_text,"_blank");
+    }
     //else
     //   alert("This link will open Whatsapp on android");
  }
